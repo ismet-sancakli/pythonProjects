f2a7a10f8e47a9d4c057ea9aee509b0492a570c5
İsmet Sancaklı <45608091+ismetSANCAKLI@users.noreply.github.com>
2021-03-06 16:20:56 +0300

    Update BlackJack_Game.py

--- BlackJack_Game.py
+++ BlackJack_Game.py
@@ -31,23 +31,23 @@ def calculate_score(cards):
 def compare(user_score, computer_score):
   #Bug fix. If you and the computer are both over, you lose.
   if user_score > 21 and computer_score > 21:
-    return "You went over. You lose 😤"
+    return "You went over. You lose "
 
 
   if user_score == computer_score:
-    return "Draw 🙃"
+    return "Draw "
   elif computer_score == 0:
-    return "Lose, opponent has Blackjack 😱"
+    return "Lose, opponent has Blackjack "
   elif user_score == 0:
-    return "Win with a Blackjack 😎"
+    return "Win with a Blackjack "
   elif user_score > 21:
-    return "You went over. You lose 😭"
+    return "You went over. You lose "
   elif computer_score > 21:
-    return "Opponent went over. You win 😁"
+    return "Opponent went over. You win "
   elif user_score > computer_score:
-    return "You win 😃"
+    return "You win "
   else:
-    return "You lose 😤"
+    return "You lose "
 
 def play_game():
 
@@ -55,27 +55,27 @@ def play_game():
 
   user_cards = []
   computer_cards = []
-  is_game_over = False
+  is_game_over = True
 
   for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
 
-  while not is_game_over:
+  while is_game_over:
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
     print(f"   Your cards: {user_cards}, current score: {user_score}")
     print(f"   Computer's first card: {computer_cards[0]}")
 
     if user_score == 0 or computer_score == 0 or user_score > 21:
-      is_game_over = True
+      is_game_over = False
     else:
       user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
       if user_should_deal == "y":
         user_cards.append(deal_card())
       else:
-        is_game_over = True
+        is_game_over = False
 
   while computer_score != 0 and computer_score < 17:
     computer_cards.append(deal_card())
